2d7e16f9fa215321ba438da783fce210144973f8
xiyeqing <963089662@qq.com>
2017-02-13 15:43:26 +0800

    9 commit

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -6,6 +6,6 @@ public class Test {
     public static void main(String[] args) {
         Logger logger = Logger.getLogger(Test.class);
         logger.info("111");
-        System.out.println("555");
+        System.out.println("666");
     }
 }
