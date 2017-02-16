08c906fad06b001b96176909b54204c030d4269e
xiyeqing <963089662@qq.com>
2017-02-16 16:24:51 +0800

    18 commit

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,11 +1,8 @@
 
-import org.apache.log4j.Logger;
-
 
 public class Test {
     public static void main(String[] args) {
-        Logger logger = Logger.getLogger(Test.class);
-        logger.info("111");
+
         System.out.println("1005");
     }
 }
